005d2c9ef5a14240bda64465b8072cc5297f7637
Den <91518057+kark4rasev@users.noreply.github.com>
2023-09-26 13:01:49 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Main from './Components/Main.js';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import './Components/style.css';
+
+
+function App() {
+  return (
+    <>
+      <Main />
+     
+       
+      
+      
+    </>
+  );
+}
+
+export default App;
